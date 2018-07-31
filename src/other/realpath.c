5f4bd875c5e363a93ccc0844a726cba4b41acb4a
Michael Macias <michael.macias@stjude.org>
2018-07-31 11:17:57 -0500

    Require arguments for realpath
    Although cis-X does not call realpath without arguments, this is to
    prevent it from crashing when doing so. It is, however, still just a
    simple shim, returning `(null)` if the pathname does not exist.

--- src/other/realpath.c
+++ src/other/realpath.c
@@ -2,8 +2,16 @@
 #include <stdlib.h>
 
 int main(int argc, char **argv) {
-    char *rp = realpath(argv[1], NULL);
-    printf("%s\n", rp);
-    free(rp);
-    return 0;
+    if (argc < 2) {
+        fprintf(stderr, "%s: missing operand\n", argv[0]);
+        return EXIT_FAILURE;
+    }
+
+    for (int i = 1; i < argc; i++) {
+        char *rp = realpath(argv[i], NULL);
+        printf("%s\n", rp);
+        free(rp);
+    }
+
+    return EXIT_SUCCESS;
 }
