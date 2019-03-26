34387b8931d2fc1c6a7a54514c8ae41199c80133
Michael Macias <michael.macias@stjude.org>
2019-03-26 16:40:51 -0500

    core: Reevaluate known oncogenes in COSMIC

--- src/core/src/ase.candidate.pl
+++ src/core/src/ase.candidate.pl
@@ -11,8 +11,9 @@ my $ase_result_gene      = $ARGV[7];
 my $ohe_result           = $ARGV[8];
 my $thresh_loo_hi_perc   = $ARGV[9];
 my $imprinting_genes     = $ARGV[10];
+my $oncogenes            = $ARGV[11];
 
-my (%imprint,%g2loo,%glst);
+my (%imprint,%g2loo,%glst,%oncog);
 
 my $infile = $imprinting_genes;
 open IN, "< $infile" or die "$infile: $!";
@@ -21,6 +22,20 @@ while(<IN>) {
     next if $. == 1;
     my @F = split/\t/;
     $imprint{$F[0]} = $F[3];
+}
+close IN;
+
+$infile = $oncogenes;
+open IN, "< $infile" or die "$infile: $!";
+while(<IN>) {
+    chomp;
+    next if $. == 1;
+    $_ =~ s/\"//g;
+    my @F = split/\t/;
+    next unless $F[14];
+    next if $F[14] eq "TSG";
+    next if $F[0] eq "IGH" or $F[0] eq "IGK" or $F[0] eq "IGL" or $F[0] eq "HLA-A";
+    $oncog{$F[0]} = $F[14];
 }
 close IN;
 
@@ -56,18 +71,28 @@ open IN, "< $infile" or die "$infile: $!";
 while(<IN>) {
     chomp;
     if ($. == 1) {
-        print OUT "$_\tFPKM\tloo.source\tloo.cohort.size\tloo.pval\tloo.rank\timprinting.status\tcandidate.group\n";
+        print OUT "$_\tFPKM\tloo.source\tloo.cohort.size\tloo.pval\tloo.rank\timprinting.status\tcandidate.group\tdescription\n";
         next;
     }
     my @F = split/\t/;
     my $imprint = "";
     my $candidate_group = "";
+    my $description = "";
     if ($imprint{$F[1]}) {
         $imprint = $imprint{$F[1]};
     }
+    next if $imprint eq "Imprinted";
     next unless $g2loo{$F[1]};
     $glst{$F[1]} = 1;
-    next unless $g2loo{$F[1]}{fpkm} >= $thresh_fpkm;
+    ### 2018/12/25, for the known oncogenes in cosmic, keep for next step if p-value pass the threshold and fpkm >= 1.
+    if ($g2loo{$F[1]}{fpkm} >= $thresh_fpkm) {
+        1;
+    }elsif ($oncog{$F[1]} and $g2loo{$F[1]}{fpkm} >= 1) {
+        $description = "rescued-ohe";
+    }else {
+        next;
+    }
+#    next unless $g2loo{$F[1]}{fpkm} >= $thresh_fpkm;
     my $tag = $F[16];
     my @tag = split(/,/,$tag);
     my $tagnum = scalar(@tag);
@@ -82,10 +107,10 @@ while(<IN>) {
         $class = "cnvloh";
     }
 #    next unless $g2loo{$F[1]}{pval} < $thresh_loo_pvalue;
-    if ($F[21] < $thresh_ase_pvalue) {
+    if ($F[22] < $thresh_ase_pvalue) {
         if ($class eq "cnvloh") {
             ### use $thresh_ase_delta_cnv if > 30% of the markers sits in cnvloh region.
-            if ($F[18] >= $thresh_ase_delta_cnv) {
+            if ($F[19] >= $thresh_ase_delta_cnv) {
                 if ($g2loo{$F[1]}{pval} < $thresh_loo_pvalue) {
                     $candidate_group = "ase_outlier";
                 }elsif ($g2loo{$F[1]}{rank}/$g2loo{$F[1]}{size} <= $thresh_loo_hi_perc) {
@@ -101,7 +126,7 @@ while(<IN>) {
                 }
             }
         }else {
-            if ($F[18] >= $thresh_ase_delta_di) {
+            if ($F[19] >= $thresh_ase_delta_di) {
                 if ($g2loo{$F[1]}{pval} < $thresh_loo_pvalue) {
                     $candidate_group = "ase_outlier";
                 }elsif ($g2loo{$F[1]}{rank}/$g2loo{$F[1]}{size} <= $thresh_loo_hi_perc) {
@@ -117,9 +142,71 @@ while(<IN>) {
                 }
             }
         }
+        if ($oncog{$F[1]}) {
+            if ($description) {
+                $description = "$oncog{$F[1]}, $description";
+            }else {
+                $description = "$oncog{$F[1]}";
+            }
+        }
+    }else {
+        ### for the known oncogenes in cosmic, rescue if raw p-value < 0.05 && all markers show mono-allelic transcription (maf-rna < 0.05 || maf-rna > 0.95).
+        my $keep = 1;
+        if ($oncog{$F[1]} and $F[20] < 0.05) {
+            my @mafrna = split(/,/,$F[17]);
+            for my $f (@mafrna) {
+                if ($f > 0.95 or $f < 0.05) {
+                    1;
+                }else {
+                    $keep = 0;
+                }
+            }
+        }else {
+            $keep = 0;
+        }
+        if ($keep == 1) {
+            if ($class eq "cnvloh") {
+            ### use $thresh_ase_delta_cnv if > 30% of the markers sits in cnvloh region.
+                if ($F[19] >= $thresh_ase_delta_cnv) {
+                    if ($g2loo{$F[1]}{pval} < $thresh_loo_pvalue) {
+                        $candidate_group = "ase_outlier";
+                    }elsif ($g2loo{$F[1]}{rank}/$g2loo{$F[1]}{size} <= $thresh_loo_hi_perc) {
+                        $candidate_group = "ase_high";
+                    }else {
+                        1;
+                    }
+                }else {
+                    if ($g2loo{$F[1]}{pval} < $thresh_loo_pvalue) {
+                        $candidate_group = "uncertain_outlier";
+                    }else {
+                        1;
+                    }
+                }
+            }else {
+                if ($F[19] >= $thresh_ase_delta_di) {
+                    if ($g2loo{$F[1]}{pval} < $thresh_loo_pvalue) {
+                        $candidate_group = "ase_outlier";
+                    }elsif ($g2loo{$F[1]}{rank}/$g2loo{$F[1]}{size} <= $thresh_loo_hi_perc) {
+                        $candidate_group = "ase_high";
+                    }else {
+                        1;
+                    }
+                }else {
+                    if ($g2loo{$F[1]}{pval} < $thresh_loo_pvalue) {
+                        $candidate_group = "uncertain_outlier";
+                    }else {
+                        1;
+                    }
+                }
+            }
+            if ($description) {
+                $description = "$oncog{$F[1]}, rescued-ase, $description";
+            }else {
+                $description = "$oncog{$F[1]}, rescued-ase";
+            }
+        }
     }
     next unless $candidate_group eq "ase_outlier";
-    print OUT "$_\t$g2loo{$F[1]}{fpkm}\t$g2loo{$F[1]}{source}\t$g2loo{$F[1]}{size}\t$g2loo{$F[1]}{pval}\t$g2loo{$F[1]}{rank}\t$imprint\t$candidate_group\n";
+    print OUT "$_\t$g2loo{$F[1]}{fpkm}\t$g2loo{$F[1]}{source}\t$g2loo{$F[1]}{size}\t$g2loo{$F[1]}{pval}\t$g2loo{$F[1]}{rank}\t$imprint\t$candidate_group\t$description\n";
 }
 close IN;
-
